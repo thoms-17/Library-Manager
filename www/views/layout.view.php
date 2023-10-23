3b8287de61aa4ec843250ceff0e004639d73a6d6
Thom's <thomcooper04@gmail.com>
2023-10-23 21:51:12 +0200

    Correction navbar avec bootstrap

--- www/views/layout.view.php
+++ www/views/layout.view.php
@@ -3,21 +3,18 @@
 
 <head>
     <title>Mon site</title>
-    <link rel="stylesheet" href="/styles/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/home">Mon Site</a>
-
-        <!-- Icône d'utilisateur avec menu déroulant -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <a class="navbar-brand text-white" href="/home">Mon Site</a>
         <div class="nav-item dropdown ml-auto">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-user"></i>
+                <i class="fas fa-user text-white"></i>
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                 <?php if (isset($_SESSION['user_id'])) : ?>
                     <a class="dropdown-item" href="/account">Mon Compte</a>
                     <a class="dropdown-item" href="/logout">Déconnexion</a>
@@ -29,7 +26,6 @@
         </div>
     </nav>
 
-    <!-- Votre contenu de page ici -->
 </body>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
--- www/views/user_account.view.php
+++ www/views/user_account.view.php
@@ -4,20 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Mon Compte</title>
-    <link rel="stylesheet" href="../styles/user_account.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
-    <h1>Mon Compte</h1>
-    <?php if (isset($userData)) : ?>
-        <div class="card">
-            <p>Nom d'utilisateur : <?= $userData['username'] ?></p>
-            <p>Adresse e-mail : <?= $userData['email'] ?></p>
-            <p>Date d'inscription : <?= date("d/m/Y", strtotime($userData['creation_date'])) ?></p>
-        </div>
-    <?php else : ?>
-        <p>Impossible de récupérer les informations de l'utilisateur.</p>
-    <?php endif; ?>
+    <div class="container my-5">
+        <h1 class="mt-4 text-center">Mon Compte</h1>
+        <?php if (isset($userData)) : ?>
+            <div class="card mt-4 mx-auto" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Informations Utilisateur</h5>
+                    <p class="card-text">Nom d'utilisateur : <?= $userData['username'] ?></p>
+                    <p class="card-text">Adresse e-mail : <?= $userData['email'] ?></p>
+                    <p class="card-text">Date d'inscription : <?= date("d/m/Y", strtotime($userData['creation_date'])) ?></p>
+                </div>
+            </div>
+        <?php else : ?>
+            <p>Impossible de récupérer les informations de l'utilisateur.</p>
+        <?php endif; ?>
+    </div>
+    <!-- Inclure les scripts Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
